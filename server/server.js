d0d6ea8aa7ba7fbd186061f64aa7d3f394300bd5
Chase Young <chasekyoung@gmail.com>
2022-04-01 16:30:53 -0400

    commented out routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
